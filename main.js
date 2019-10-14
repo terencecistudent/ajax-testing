be00e8db7c3479e36392a24e903d4488e0515e63
terencecistudent <terencelogue100@hotmail.co.uk>
2019-10-14 10:20:43 +0000

    initial commit

--- main.js
+++ main.js
@@ -1,20 +1,61 @@
-/* XMLHttpRequest object is an inbuilt object that JavaScript provides
-to allow us to consume APIs. */
-// Gives us the method to open connections, to send connections, and close them.
-var xhr = new XMLHttpRequest();
+const baseURL = "https://www.swapi.co/api/";
 
-var data;
+function getData(type, cb) {
+    /* XMLHttpRequest object is an inbuilt object that JavaScript provides
+    to allow us to consume APIs. */
+    // Gives us the method to open connections, to send connections, and close them.
+    var xhr = new XMLHttpRequest();
 
-// Opening connection.
-xhr.open("GET", "https://www.swapi.co/api/");
+    // Opening connection.
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
 
-xhr.send();
+    // Change that by JSON.parse(); - check console in inspect.
+    xhr.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            // cb stands for callbacks.
+            cb(JSON.parse(this.responseText));
+        }
+    };
+}
 
-// Is not json, is a string.
-// Change that by JSON.parse(); - check console in inspect.
-xhr.onreadystatechange = function() {
-    if(this.readyState == 4 && this.status == 200) {
-        data = this.responseText;
-    }
-};
-console.log(data);
+function getTableHeader(obj) {
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`);
+    });
+
+    return `<tr>${tableHeaders}</tr>`;
+}
+
+// Parameter type means type from api.
+// console.dir - dir stands for directory.
+function writeToDocument(type) {
+    var tableRows = [];
+    var el = document.getElementById("data");
+    el.innerHTML = "";
+
+    getData(type, function(data) {
+        data = data.results;
+        var tableHeaders = getTableHeader(data[0]);
+
+        data.forEach(function(item) {
+            var dataRow = [];
+
+            Object.keys(item).forEach(function(key) {
+                // Going to be set to the value of the key and will be a string.
+                var rowData = item[key].toString();
+
+                // Going to be equal to a substring of our rowData.
+                // Will just take the first 15 characters from our rowData.
+                var truncatedData = rowData.substring(0, 15);
+
+                dataRow.push(`<td>${truncatedData}</td>`);
+            })
+            tableRows.push(`<tr>${dataRow}</tr>`);
+        });
+
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+    });
+}
