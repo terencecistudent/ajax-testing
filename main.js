2fbe634b2cb00638b4bb9e33777e6499592dfba0
terencecistudent <terencelogue100@hotmail.co.uk>
2019-10-14 11:03:44 +0000

    fix big with commas rendering at the top of the table

--- main.js
+++ main.js
@@ -78,6 +78,6 @@ function writeToDocument(url) {
             tableRows.push(`<tr>${dataRow}</tr>`);
         });
 
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`.replace(/,/g, "");
     });
 }
